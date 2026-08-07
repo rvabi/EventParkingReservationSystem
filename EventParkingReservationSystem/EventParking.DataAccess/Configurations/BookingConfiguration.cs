655c403545a2a699776e3da328f80ab4fb8fa0c9
rvabi <abi47993@gmail.com>
2026-08-07 09:42:46 +0530

    feat: add seat parking and booking entities

---
+++ EventParkingReservationSystem/EventParking.DataAccess/Configurations/BookingConfiguration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventParking.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventParking.DataAccess.Configurations;
+
+public class BookingConfiguration
+    : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("Bookings", table =>
+        {
+            table.HasCheckConstraint(
+                "CK_Bookings_TotalAmount_NonNegative",
+                "[TotalAmount] >= 0");
+        });
+
+        builder.HasKey(booking => booking.Id);
+
+        builder.Property(booking => booking.BookingNumber)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(booking => booking.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(booking => booking.HoldExpiresAt)
+            .IsRequired(false);
+
+        builder.Property(booking => booking.TotalAmount)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(booking => booking.CancelledAt)
+            .IsRequired(false);
+
+        builder.Property(booking => booking.CreatedAt)
+            .IsRequired();
+
+        builder.Property(booking => booking.UpdatedAt)
+            .IsRequired(false);
+
+        builder.HasOne(booking => booking.Customer)
+            .WithMany(customer => customer.Bookings)
+            .HasForeignKey(booking => booking.CustomerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(booking => booking.Event)
+            .WithMany(eventItem => eventItem.Bookings)
+            .HasForeignKey(booking => booking.EventId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(booking => booking.BookingNumber)
+            .IsUnique()
+            .HasDatabaseName(
+                "UX_Bookings_BookingNumber");
+
+        builder.HasIndex(booking => new
+        {
+            booking.CustomerId,
+            booking.CreatedAt
+        })
+        .HasDatabaseName(
+            "IX_Bookings_CustomerId_CreatedAt");
+
+        builder.HasIndex(booking => new
+        {
+            booking.Status,
+            booking.HoldExpiresAt
+        })
+        .HasDatabaseName(
+            "IX_Bookings_Status_HoldExpiresAt");
+    }
+}
---
+++ EventParkingReservationSystem/EventParking.DataAccess/Configurations/BookingSeatConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventParking.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventParking.DataAccess.Configurations;
+
+public class BookingSeatConfiguration
+    : IEntityTypeConfiguration<BookingSeat>
+{
+    public void Configure(
+        EntityTypeBuilder<BookingSeat> builder)
+    {
+        builder.ToTable("BookingSeats");
+
+        builder.HasKey(bookingSeat => bookingSeat.Id);
+
+        builder.Property(bookingSeat => bookingSeat.CreatedAt)
+            .IsRequired();
+
+        builder.Property(bookingSeat => bookingSeat.UpdatedAt)
+            .IsRequired(false);
+
+        builder.HasOne(bookingSeat => bookingSeat.Booking)
+            .WithMany(booking => booking.BookingSeats)
+            .HasForeignKey(bookingSeat => bookingSeat.BookingId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(bookingSeat => bookingSeat.Seat)
+            .WithMany(seat => seat.BookingSeats)
+            .HasForeignKey(bookingSeat => bookingSeat.SeatId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(bookingSeat => new
+        {
+            bookingSeat.BookingId,
+            bookingSeat.SeatId
+        })
+        .IsUnique()
+        .HasDatabaseName(
+            "UX_BookingSeats_BookingId_SeatId");
+
+        builder.HasIndex(bookingSeat => bookingSeat.SeatId)
+            .HasDatabaseName(
+                "IX_BookingSeats_SeatId");
+    }
+}
