ebc8783ac5dd8365db47b51a2a30e4c58ef5aa7d
rvabi <abi47993@gmail.com>
2026-08-07 08:39:10 +0530

    feat: add shared base entity and core enums

---
+++ EventParkingReservationSystem/EventParking.Models/Enums/CustomerStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventParking.Models.Enums;
+
+public enum CustomerStatus
+{
+    Active = 1,
+    Deactivated = 2
+}
