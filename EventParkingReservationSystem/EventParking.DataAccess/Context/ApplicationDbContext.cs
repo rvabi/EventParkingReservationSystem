6d7bbbe27242ff47364481fb966e1ad08f48b2ac
rvabi <abi47993@gmail.com>
2026-08-07 09:30:21 +0530

    feat: add EF Core database context foundation

---
+++ EventParkingReservationSystem/EventParking.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EventParking.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventParking.DataAccess.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; } = null!;
+
+    public DbSet<Venue> Venues { get; set; } = null!;
+
+    public DbSet<EventCategory> EventCategories { get; set; } = null!;
+
+    public DbSet<Event> Events { get; set; } = null!;
+
+    public DbSet<Seat> Seats { get; set; } = null!;
+
+    public DbSet<ParkingSlot> ParkingSlots { get; set; } = null!;
+
+    public DbSet<Booking> Bookings { get; set; } = null!;
+
+    public DbSet<BookingSeat> BookingSeats { get; set; } = null!;
+
+    public DbSet<ParkingReservation> ParkingReservations { get; set; } = null!;
+
+    public DbSet<Payment> Payments { get; set; } = null!;
+
+    public DbSet<Notification> Notifications { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
