032d5444db8f008c526919f471c2ce3b16dfbcc0
rvabi <abi47993@gmail.com>
2026-08-07 10:22:34 +0530

    feat: configure SQL Server database connection

--- EventParkingReservationSystem/EventParking.Api/Program.cs
+++ EventParkingReservationSystem/EventParking.Api/Program.cs
@@ -1,15 +1,25 @@
+using EventParking.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException(
+        "DefaultConnection is not configured.");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
