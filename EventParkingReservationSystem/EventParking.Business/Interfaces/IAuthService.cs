41c0be6388654a5af9fa41e0edeaa5489e255217
Roshni <roshnikulas08@gmail.com>
2026-08-07 16:11:22 +0530

    feat(auth): implement registration and email verification

---
+++ EventParkingReservationSystem/EventParking.Business/Interfaces/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventParking.Business.DTOs.Auth;
+
+
+namespace EventParking.Business.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResult> RegisterAsync(RegisterRequest request);
+
+    Task<AuthResult> VerifyEmailAsync(string token);
+
+    Task<AuthResult> ResendVerificationAsync(
+        ResendVerificationRequest request);
+}
