21b66904dce2da1328bed01af2a84dbe95112c82
Thiyagaraja Yumesha Shalini <UT011312@unicomtic.com>
2026-08-08 14:18:34 +0530

    feat: add food order request and response DTOs

---
+++ EventParkingReservationSystem/EventParking.Business/DTOs/FoodCourt/FoodOrderItemResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace EventParking.Business.DTOs.FoodCourt;
+
+public class FoodOrderItemResponse
+{
+    public int Id { get; set; }
+
+    public int FoodItemId { get; set; }
+
+    public string FoodItemName { get; set; } = string.Empty;
+
+    public int Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public decimal LineTotal { get; set; }
+}
