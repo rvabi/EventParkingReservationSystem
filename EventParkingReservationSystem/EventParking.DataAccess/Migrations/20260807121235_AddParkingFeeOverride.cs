0ba1cf353b02924e71bd02fd356f178b5a933130
Thiyagaraja Yumesha Shalini <UT011312@unicomtic.com>
2026-08-08 13:04:00 +0530

    feat: add food court database migration

--- EventParkingReservationSystem/EventParking.DataAccess/Migrations/20260807121235_AddParkingFeeOverride.cs
+++ EventParkingReservationSystem/EventParking.DataAccess/Migrations/20260807121235_AddParkingFeeOverride.cs
@@ -4,35 +4,17 @@
 
 namespace EventParking.DataAccess.Migrations
 {
-    /// <inheritdoc />
     public partial class AddParkingFeeOverride : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<decimal>(
-     name: "FeeOverride",
-     table: "ParkingSlots",
-     type: "decimal(18,2)",
-     nullable: true);
-
-
-            migrationBuilder.AddCheckConstraint(
-    name: "CK_ParkingSlots_FeeOverride_NonNegative",
-    table: "ParkingSlots",
-    sql: "[FeeOverride] IS NULL OR [FeeOverride] >= 0");
+            // No operation.
+            // FeeOverride was already added by AddFeeOverrideToParkingSlots.
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropCheckConstraint(
-                name: "CK_ParkingSlots_FeeOverride_NonNegative",
-                table: "ParkingSlots");
-
-            migrationBuilder.DropColumn(
-                name: "FeeOverride",
-                table: "ParkingSlots");
+            // No operation.
         }
     }
 }
--- EventParkingReservationSystem/EventParking.DataAccess/Migrations/20260808071010_AddFoodCourtEntities.Designer.cs
+++ EventParkingReservationSystem/EventParking.DataAccess/Migrations/20260808071010_AddFoodCourtEntities.Designer.cs
@@ -4,6 +4,7 @@ using EventParking.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventParking.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260808071010_AddFoodCourtEntities")]
+    partial class AddFoodCourtEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
